81b76b9f64a91ce0f90fe2eeac9adab37ac79bd1
Steve Scruff <mutantpiggiegolems@gmail.com>
2023-04-02 20:21:00 -0700

    debugging

--- src/discord/voicecmds/voicecmds.ts
+++ src/discord/voicecmds/voicecmds.ts
@@ -16,14 +16,16 @@ export function onJoin(channel: VoiceBasedChannel): VoiceConnection {
     const m = channel.members.find(m=>WHITELIST.has(m.id));
     if (!m) return join(channel);
     const connection = join(channel, false);
-    connection.receiver.subscribe(m.id)
+    
+    const pipe = connection.receiver.subscribe(m.id)
     .pipe(new prism.opus.Decoder({frameSize: 960, channels: 2, rate: 48000}))
     .pipe(new prism.FFmpeg({args: [//"-analyzeduration", "0", "-loglevel", "0",
         '-f', 's16le',
         '-ar', rhino.sampleRate.toString(),
         '-ac', '1',
-    ]})) // FIXME: This is bad
-    .on("data", (buf: Buffer) => {
+    ]})); // FIXME: This is bad
+    pipe.on("error", console.error)
+    pipe.on("data", (buf: Buffer) => {
         console.info("Audio Packet 2")
         if (rhino.process(new Int16Array(buf.buffer))) {
             const inf = rhino.getInference();
@@ -39,6 +41,6 @@ export function onJoin(channel: VoiceBasedChannel): VoiceConnection {
                     console.error(`Voice Command Error: Unrecognized Intent [${inf.intent}]`)
             }
         };
-    }).on("error", console.error);
+    })
     return connection;
 }
