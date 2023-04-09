3f7b7d4067038ae620a07305c7668a53ba679a91
Steve Scruff <mutantpiggiegolems@gmail.com>
2023-04-09 15:24:33 -0700

    skip the middleman for randomness

--- src/recommendation/index.py
+++ src/recommendation/index.py
@@ -1,5 +1,4 @@
-import sys
-import os
-from random import randint, seed
-seed(os.urandom(24))
-print(randint(*[int(i) for i in sys.argv[1:3]]))
+import sys, os, math
+min, max = int(sys.argv[1]), int(sys.argv[2])
+size = math.floor((max-min)**0.4)
+print(math.floor((int.from_bytes(os.urandom(size))/(0xff**size))*(max-min))+min)
