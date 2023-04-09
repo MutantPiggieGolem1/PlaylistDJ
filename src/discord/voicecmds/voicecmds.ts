12f5e7db99fc36d4c508d39288d83a5ba93673e6
Steve Scruff <mutantpiggiegolems@gmail.com>
2023-04-09 15:27:40 -0700

    fix implementation, now rhino's fault

--- src/discord/voicecmds/voicecmds.ts
+++ src/discord/voicecmds/voicecmds.ts
@@ -1,4 +1,4 @@
-import { EndBehavior, VoiceConnection } from "@discordjs/voice";
+import { VoiceConnection } from "@discordjs/voice";
 import { Rhino } from "@picovoice/rhino-node";
 import { VoiceBasedChannel } from "discord.js";
 import fs from "fs";
@@ -7,7 +7,6 @@ import { WHITELIST } from "../../index";
 import { join } from "../commands/join";
 import { leave } from "../commands/leave";
 import { play } from "../commands/play";
-import { EndBehaviorType } from "@discordjs/voice";
 const models: any = {
     "win32": "dj_en_windows_v2_1_0.rhn"
 }
@@ -19,39 +18,45 @@ export function onJoin(channel: VoiceBasedChannel): VoiceConnection {
     const connection = join(channel, false);
     
     const ffmpegInst = new prism.FFmpeg({args: [
-        '-f', 'opus',
+        '-f', 's16le',
         '-i', '-',
         '-analyzeduration', '0',
-        '-loglevel', '40',
+        '-loglevel', 'fatal',
         '-hide_banner',
         '-f', 's16le',
         '-c:a', 'pcm_s16le',
         '-ar', rhino.sampleRate.toString(),
-        '-frame_size', rhino.frameLength.toString(),
         '-ac', '1',
     ]});
-    ffmpegInst.process.stdout?.on("data", a => console.log(a.toString()))
-    ffmpegInst.process.stderr?.on("data", a => console.error(a.toString()));
-    const pipe = connection.receiver.subscribe(m.id, {"autoDestroy": true, "end": {behavior: EndBehaviorType.AfterSilence, duration: 2}})
+    ffmpegInst.process.stderr?.on("data", a => console.error(a.toString()))
+    const pipe = connection.receiver.subscribe(m.id)
     .pipe(new prism.opus.Decoder({frameSize: 960, channels: 2, rate: 48000}))
     .pipe(ffmpegInst);
     pipe.on("error", console.error)
-    pipe.on("data", (buf: Buffer) => {
-        if (buf.buffer.byteLength !== rhino.frameLength) return console.warn(`Buffer Mis-Sized! ${buf.buffer.byteLength} != ${rhino.frameLength}`)
-        if (rhino.process(new Int16Array(buf.buffer))) {
-            const inf = rhino.getInference();
-            if (!inf.isUnderstood) return;
-            switch (inf.intent) {
-                case "play":
-                    if (!play(channel.guild.id)) console.warn("Voice Command Error: Failed to play.")
-                break;
-                case "leave":
-                    leave(channel.guild.id);
-                break;
-                default:
-                    console.error(`Voice Command Error: Unrecognized Intent [${inf.intent}]`)
-            }
-        };
+    let chunks: Uint16Array;
+    pipe.on("data", (chunk: Buffer) => {
+        chunks = new Uint16Array([...(chunks??[]), ...new Uint16Array(chunk.buffer)]);
+        while (chunks.length > rhino.frameLength) {
+            onData(new Int16Array(chunks.slice(0, rhino.frameLength)), channel.guild.id)
+            chunks = chunks.slice(rhino.frameLength)
+        }
     })
     return connection;
+}
+
+function onData(arr: Int16Array, guildID: string) {
+    if (rhino.process(arr)) {
+        const inf = rhino.getInference();
+        if (!inf.isUnderstood) return console.error("Voice Command Error: Misunderstood "+inf.intent);
+        switch (inf.intent) {
+            case "play":
+                if (!play(guildID)) console.warn("Voice Command Error: Failed to play.")
+            break;
+            case "leave":
+                leave(guildID);
+            break;
+            default:
+                console.error(`Voice Command Error: Unrecognized Intent [${inf.intent}]`)
+        }
+    };
 }
